6e617289bfe2f8f39225c50da337fa7686a328f7
Frank Francis <ryangoosen7@gmail.com>
2023-06-30 19:25:30 +0200

    update

---
+++ mavenproject2/src/main/java/lms/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lms.mavenproject2;
+
+/**
+ *
+ * @author frank
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ mavenproject3/src/main/java/lms/mavenproject3/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lms.mavenproject3;
+
+/**
+ *
+ * @author frank
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
